e48f7656c82bb09de03a951ef2c846403da519f6
Lemeng <lyangithub.gmail.com>
2024-10-27 16:11:40 -0700

    https://leetcode.com/problems/merge-sorted-array/submissions/1435648823

---
+++ Leetcode88.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+/**
+ * Leetcode88
+ */
+public class Leetcode88 {
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] <= nums2[j]) {
+                nums1[k--] = nums2[j--];
+            } else {
+                nums1[k--] = nums1[i--];
+            }
+        }
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+        int[] nums2 = { 2, 5, 6 };
+        merge(nums1, 3, nums2, 3);
+        System.err.println(Arrays.toString(nums1));
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # leetcode
 my leetcode 
+
+## 88. Merge Sorted Array
+    Type: Array
+    merge array forward or reverse, forward need to creat a new array to store, reverse not need.
+    also can use api System.arraycopy, it is a native method by memmove
